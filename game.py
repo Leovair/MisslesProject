f7cc140dc6cbb82a99786115ee49128c24edcc9b
Leovair <Leovair@users.noreply.github.com>
2018-12-27 20:26:00 +0200

    Изменение структуры, переделав в класс, сжатие кода.

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 import random
 import turtle
-import math
 import os
 
 BASE_PATH = os.path.dirname(__file__)
@@ -11,27 +10,66 @@ window.setup(1200 + 10, 800 + 10)
 window.screensize(1200, 800)
 window.tracer(n=2)
 
-
-
 # Базовые переменные для расположения и работы с ракетами вражескими и игрока
 BASE_X = 0
 BASE_Y = -300
 # ENEMY_BASE_X_MIN = -600
 # ENEMY_BASE_X_MAX = 600
 # ENEMY_BASE_Y = 400
-ENEMY_COUNT = 5  # 5 вражеских ракет, для контроля ракет летящих на игрока
+ENEMY_COUNT = 1  # 5 вражеских ракет, для контроля ракет летящих на игрока
 
+'''
+Создадим класс Missiles, в котором будет наш объект ракеты. 
+'''
+class Missile:
+    def __init__(self, x, y, color, x2, y2):
+        # self.x = x
+        # self.y = y
+        self.color = color
+        pen = turtle.Turtle(visible=False)
+        pen.color(color)
+        pen.speed(0)
+        pen.penup()
+        pen.setpos(x=x, y=y)
+        pen.pendown()
+        heading_rocket = pen.towards(x2, y2)
+        pen.setheading(heading_rocket)
+        pen.showturtle()
+        self.pen = pen
 
-def calculate_heading(x1, y1, x2, y2):
-    dx = x2 - x1
-    dy = y2 - y1
-    lenght = (dx ** 2 + dy ** 2) ** 0.5
-    cos_alpha = dx / lenght
-    alpha = math.acos(cos_alpha)
-    alpha = math.degrees(alpha)
-    if dy < 0:
-        alpha = -alpha
-    return alpha
+        self.state = 'launched'
+        self.target = x2, y2
+        self.radius = 0
+
+    def step(self):
+        if self.state == 'launched':
+            self.pen.forward(4)
+            if self.pen.distance(x=self.target[0], y=self.target[1]) < 20:
+                self.state = 'explode'
+                self.pen.shape('circle')
+        elif self.state == 'explode':
+            self.radius += 1
+            # check radius, and change rocket status
+            if self.radius > 5:
+                self.pen.clear()
+                self.pen.hideturtle()
+                self.state = 'dead'
+            else:
+                self.pen.shapesize(self.radius)
+        elif self.state == 'dead':
+            self.pen.clear()
+            self.pen.hideturtle()
+
+    def distance(self, x, y):
+        return self.pen.distance(x=x, y=y)
+
+    @property
+    def x(self):
+        return self.pen.xcor()
+
+    @property
+    def y(self):
+        return self.pen.ycor()
 
 
 '''
@@ -39,85 +77,86 @@ def calculate_heading(x1, y1, x2, y2):
 для сжатия кода, не создавания двух функций практически дублирующих друг друга
 '''
 def create_missile(color, x, y, x2, y2):
-    missile = turtle.Turtle(visible=False)
-    missile.color(color)
-    missile.hideturtle()
-    missile.speed(0)
-    missile.penup()
-    missile.setpos(x=x, y=y)
-    missile.pendown()
-    heading_rocket = missile.towards(x2, y2)  # заменяем calculate_heading(x1=x, y1=ENEMY_BASE_Y, x2=BASE_X, y2=BASE_Y) на функцию towards()
-    missile.setheading(heading_rocket)
-    missile.showturtle()
-    info_about_rockets = {'missile': missile, 'target': [x2, y2],
-                          'state': 'launched', 'radius': 0}
-    return info_about_rockets
+    missile = Missile(x=x, y=y, color=color, x2=x2, y2=y2)
+    return missile
 
 
 def fire_missile(x, y):
-    info_about_rocket = create_missile(color='white', x=BASE_X, y=BASE_Y, x2=x, y2=y)
+    info_about_rocket = Missile(color='white', x=BASE_X, y=BASE_Y, x2=x, y2=y)
     our_missiles.append(info_about_rocket)
 
+
 def fire_enemy_missile():
-    x= random.randint(-600,600)
+    x = random.randint(-600, 600)
     y = 400
-    info_about_rocket = create_missile(color='red', x=x, y=y, x2=BASE_X, y2=BASE_Y)
+    info_about_rocket = Missile(color='red', x=x, y=y, x2=BASE_X, y2=BASE_Y)
     enemy_missiles_rocket.append(info_about_rocket)
 
+
 def move_missiles(missiles):
-    for info_about_rocket in missiles:
-        state = info_about_rocket['state']
-        missile = info_about_rocket['missile']
-        if state == 'launched':
-            missile.forward(4)
-            target = info_about_rocket['target']
-            if missile.distance(x=target[0], y=target[1]) < 20:
-                info_about_rocket['state'] = 'explode'
-                missile.shape('circle')
-        elif state == 'explode':
-            info_about_rocket['radius'] += 1
-            # check radius, and change rocket status
-            if info_about_rocket['radius'] > 5:
-                info_about_rocket['state'] = 'dead'
-                missile.clear()
-                missile.hideturtle()
-            else:
-                missile.shapesize(info_about_rocket['radius'])
-        elif state == 'dead':
-            missile.clear()
-            missile.hideturtle()
-        # elif state == 'dead':  # if rocket is make BOOM
+    for missile in missiles:
+        missile.step()
+    # elif state == 'dead':  # if rocket is make BOOM
     # уничтожаем ракеты игрока, чтобы не захламлять список
-    dead_missiles = [info_about_rocket for info_about_rocket in missiles if
-                     info_about_rocket['state'] == 'dead']  # Списовая сборка
+    dead_missiles = [missile for missile in missiles if
+                     missile.state == 'dead']  # Списовая сборка
     for dead in dead_missiles:
         missiles.remove(dead)
-
 
 
 def check_enemy_count():
     if len(enemy_missiles_rocket) < ENEMY_COUNT:
         fire_enemy_missile()
 
+
 def check_rocket_collision():
-    for our_info in our_missiles:
-        if our_info['state'] != 'explode':
+    for our_missile in our_missiles:
+        if our_missile.state != 'explode':
             continue
-        our_missile = our_info['missile']
-        for enemy_info in enemy_missiles_rocket:
-            enemy_missiles = enemy_info['missile']
-            if enemy_missiles.distance(our_missile.xcor(),our_missile.ycor())< 20:
-                enemy_info['state'] = 'dead'
+        for enemy_missile in enemy_missiles_rocket:
+            if enemy_missile.distance(our_missile.x, our_missile.y) < our_missile.radius * 10:
+                enemy_missile.state = 'dead'
 
 
-our_missiles = []
-enemy_missiles_rocket = []
+def our_base():
+    base = turtle.Turtle(visible=True)
+    base.hideturtle()
+    base.speed(0)
+    base.penup()
+    base.setpos(x=BASE_X, y=BASE_Y)
+    pic_path = os.path.join(BASE_PATH, 'images', 'base.gif')
+    window.register_shape(pic_path)
+    base.shape(pic_path)
+    base.showturtle()
+
+
+def game_over():
+    return base_health < 0
+
+
 window.onclick(fire_missile)
 
-# Gaming cicle, and use State pattern
-while True:
+our_missiles = []  # список наших ракет
+enemy_missiles_rocket = []  # писок вражеских ракет
+base_health = 2000
+
+
+def check_impact_to_base():
+    global base_health
+    for enemy_missile in enemy_missiles_rocket:
+        if enemy_missile.state != 'explode':
+            continue
+        if enemy_missile.distance(BASE_X, BASE_Y) < enemy_missile.radius * 10:
+            base_health -= 100
+            print('basehealth',base_health)
+
+while True:  # Gaming cicle, and use State pattern
     window.update()
+    check_impact_to_base()
+    if game_over():
+        continue
     check_enemy_count()
     check_rocket_collision()
+    our_base()
     move_missiles(missiles=our_missiles)
     move_missiles(missiles=enemy_missiles_rocket)
